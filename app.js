e372d355bdb9ffed0ac2f5c2430f38397f63ee40
Ricky Miller <ricky@metalabdesign.com>
2014-09-06 05:11:59 -0400

    working example of palettemap

--- app.js
+++ app.js
@@ -48,48 +48,93 @@ function plurnt(buffer) {
   var canvas = new Canvas(width, height)
   var ctx = canvas.getContext('2d')
 
+  var hwm = 128 * 100 * 1024 // brycebaril told me to
+  var gif = new GifEncoder(width, height, {
+    'highWaterMark': hwm
+  })
+
+  gif.setDelay(120)
+  gif.setRepeat(0)
+
+  gif.writeHeader()
+
   // draw the original image
   ctx.drawImage(img, 0, 0, width, height)
-
-  var invertMatrix = [
-    -1, 0, 0, 0, 255,
-    0, -1, 0, 0, 255,
-    0, 0, -1, 0, 255,
-    0, 0, 0, 1, 0
-  ];
-             
-  var brightness = [
-    2, 0, 0, 0, 0,
-    0, 2, 0, 0, 0,
-    0, 0, 2, 0, 0,
-    0, 0, 0, 1, 0
-  ];
-
-  var invertFilter = new ColorMatrixFilter(invertMatrix);
-  var brightnessFilter = new ColorMatrixFilter(brightness);
-
-  var zeroPoint = new Point();
 
   var bmd = new bitmapData.BitmapData(width, height)
   
   bmd.draw(img)
-  bmd.applyFilter(bmd, bmd.rect, zeroPoint, invertFilter)
+
+  var colorModifier = 1;
+  var rArray = [], gArray = [], bArray = [];
+  var point = new Point(0, 0);
 
   ctx.putImageData(bmd.data, 0, 0)
+  gif.addFrame(ctx.getImageData(0, 0, width, height).data)
 
-  var buffer = canvas.toDataURL()
-  
-  return buffer
+  var count = 0;
+  for(var j=0;j<300;j++) {
+    count++;
+    if(count===100) {
+      console.log('add frame')
+      count = 0;
+      ctx.putImageData(bmd.data, 0, 0)
+      gif.addFrame(ctx.getImageData(0, 0, width, height).data)
+    }
+    for(i=0; i<256; i++) {
+      r = i + colorModifier;
+      if(r > 255) r = r-256;
 
+      g = i + colorModifier + r;
+      if(g > 255) g = g-256;
+
+      b = i + colorModifier + g;
+      if(b > 255) b = b-256;
+
+      rArray[i] = r;
+      gArray[i] = g;
+      bArray[i] = b;
+    }
+
+    bmd.paletteMap(bmd, 
+      bmd.rect, 
+      point, 
+      rArray, 
+      gArray, 
+      bArray);
+
+    // ctx.putImageData(bmd.data, 0, 0)
+    // gif.addFrame(ctx.getImageData(0, 0, width, height).data)
+
+    colorModifier += 1;
+    if(colorModifier > 254) colorModifier = 0;
+  }
+
+  gif.finish()
+
+  // ctx.putImageData(bmd.data, 0, 0)
+  // var buffer = canvas.toDataURL()
+  // return buffer
   // will likely return gif later
+
+  return gif
 }
 
 app.post('/service', function(req, res) {
   var imgBuff = dataUriToBuffer(req.body.content.data)
-  var turnt = plurnt(imgBuff);
-  req.body.content.data = turnt;
-  req.body.content.type = imgBuff.type
-  res.json(req.body)
+  var gif = plurnt(imgBuff);
+  
+  gif.on('readable', function() {
+
+    console.log('go!')
+
+    var buffer = gif.read()
+    var dataUri = 'data:image/gif;base64,' + buffer.toString('base64')
+
+    req.body.content.data = dataUri
+    req.body.content.type = 'image/gif'
+    res.json(req.body)
+  })
 })
 
 var port = nconf.get('port')
