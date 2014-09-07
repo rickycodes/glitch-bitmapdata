641a5b5da930303970054fa238bf6089afad6ec7
Ricky Miller <ricky@metalabdesign.com>
2014-09-06 21:36:30 -0400

    Randomize the things!

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var bitmapData = require('./BitmapData.js')
 var Rectangle = bitmapData.Rectangle;
 var Point = bitmapData.Point;
 
+var ColorMatrixFilter = bitmapData.ColorMatrixFilter;
+
 var exec = require('child_process').exec
 var Canvas = require('canvas')
 var GifEncoder = require('gif-encoder')
@@ -28,10 +30,67 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-function LSD(buffer) {
-
+function setupIMG(buffer) {
   var img = new Image
   img.src = buffer
+  return img
+}
+
+function colorize(buffer) {
+  
+  var img = setupIMG(buffer)
+
+  var width = img.width
+  var height = img.height
+
+  var canvas = new Canvas(width, height)
+  var ctx = canvas.getContext('2d')
+
+  var hwm = 128 * 100 * 1024 // brycebaril told me to
+  var gif = new GifEncoder(width, height, {
+    'highWaterMark': hwm
+  })
+
+  gif.setDelay(100)
+  gif.setRepeat(0)
+
+  gif.writeHeader()
+
+  // draw the original image
+  ctx.drawImage(img, 0, 0, width, height)
+
+  var bmd = new bitmapData.BitmapData(width, height)
+
+  var zeroPoint = new Point()
+
+  for(var i =0; i < 4; i++) {
+    var matrix = [
+        2*i+1, 0, 0, 0, 0,
+        0, 2*i+1, 0, 0, 0,
+        0, 0, 2*i+1, 0, 0,
+        0, 0, 0, 1, 0
+      ];
+
+    var filter = new ColorMatrixFilter(matrix)
+    
+    bmd.draw(img)
+    bmd.applyFilter(bmd, bmd.rect, zeroPoint, filter)
+    
+    ctx.putImageData(bmd.data, 0, 0, 0, 0, width, height)  
+    
+    gif.addFrame(ctx.getImageData(0, 0, width, height).data)
+
+    console.log('addFrame')
+  }
+
+  gif.finish()
+
+  return gif
+}
+
+function LSD(buffer) {
+
+  var img = setupIMG(buffer)
 
   var width = img.width
   var height = img.height
@@ -56,13 +115,13 @@ function LSD(buffer) {
 
   bmd.draw(img)
 
-  var colorModifier = 1;
+  var colorModifier = 1
   var rArray = [],
     gArray = [],
-    bArray = [];
-  var point = new Point(0, 0);
+    bArray = []
+  var point = new Point(0, 0)
 
-  ctx.putImageData(bmd.data, 0, 0)
+  ctx.drawImage(img, 0, 0, width, height)
   gif.addFrame(ctx.getImageData(0, 0, width, height).data)
 
   var howMany = 2 // two frames, plus above
@@ -70,18 +129,18 @@ function LSD(buffer) {
   for (var j = 0; j < howMany; j++) {
 
     for (var i = 0; i < 256; i++) {
-      r = i + colorModifier;
-      if (r > 255) r = r - 256;
+      r = i + colorModifier
+      if (r > 255) r = r - 256
 
-      g = i + colorModifier + r;
-      if (g > 255) g = g - 256;
+      g = i + colorModifier + r
+      if (g > 255) g = g - 256
 
-      b = i + colorModifier + g;
-      if (b > 255) b = b - 256;
+      b = i + colorModifier + g
+      if (b > 255) b = b - 256
 
-      rArray[i] = r;
-      gArray[i] = g;
-      bArray[i] = b;
+      rArray[i] = r
+      gArray[i] = g
+      bArray[i] = b
     }
 
     bmd.paletteMap(bmd,
@@ -89,14 +148,14 @@ function LSD(buffer) {
       point,
       rArray,
       gArray,
-      bArray);
+      bArray)
 
-    colorModifier += 1;
-    if (colorModifier > 254) colorModifier = 0;
+    colorModifier += 1
+    if (colorModifier > 254) colorModifier = 0
 
     ctx.putImageData(bmd.data, 0, 0)
     gif.addFrame(ctx.getImageData(0, 0, width, height).data)
-    console.log('frame added')
+    console.log('addFrame')
   }
 
   gif.finish()
@@ -106,7 +165,13 @@ function LSD(buffer) {
 
 app.post('/service', function(req, res) {
   var imgBuff = dataUriToBuffer(req.body.content.data)
-  var gif = LSD(imgBuff);
+
+  var effects = [ colorize, LSD ]
+  var randomEffect = effects[Math.floor(Math.random()*effects.length)]
+
+  // (randomEffect)()
+
+  var gif = (randomEffect)(imgBuff)
 
   gif.on('readable', function() {
 
